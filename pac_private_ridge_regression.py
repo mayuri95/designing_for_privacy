ab9d0a782e1dd51f1e368d8179097cad8ddea662
Mayuri Sridhar <mayuri@Mayuris-MacBook-Air-2.local>
2025-10-29 14:50:25 -0400

    ridge regression updates

--- pac_private_ridge_regression.py
+++ pac_private_ridge_regression.py
@@ -11,124 +11,145 @@ from utils import *
 import sys
 from sklearn.decomposition import PCA
 
-TEST_SIZE   = 0.3
+TEST_SIZE   = 0.2
 RANDOM_SEED = 42
 NUM_SUBSETS = 128
 NUM_TRIALS = 100
 
+ # 
 C_values = [0.25, 0.5, 1.0, 2.0, 4.0, 8.0, 16.0, 32.0, 64.0, 128.0, 256.0, 512.0]
 #C_values = [C_values[int(sys.argv[1])]]
-print(C_values)
-datasets = ['wine_red', 'wine_white']
-datasets = [datasets[int(sys.argv[1])]]
-all_mses, all_lams = {}, {}
-for data in datasets:
-    if data == 'wine_red':
-        X, y = load_wine_quality(red=True)
-    elif data == 'wine_white':
-        X, y = load_wine_quality(red=False)
-    elif data == 'census':
-        X, y = load_adult_census('adult/adult.data')
-    elif data == 'bank':
-        X, y = load_bank()
+# print(C_values)
+datasets = ['wine_white', 'wine_red', 'housing']
 
-    # Train/test split and standardize X (fit on train)
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=TEST_SIZE, random_state=RANDOM_SEED
-    )
-    scaler = StandardScaler().fit(X_train)
-    X_train = scaler.transform(X_train)
-    X_test  = scaler.transform(X_test)
+lams = [('exact', 1/16), ('exact', 1/1024), ('exact', 0), 1.0, 0.1]
+# datasets = [datasets[int(sys.argv[1])]]
 
-    pca = PCA(whiten=True, random_state=RANDOM_SEED)  # orthonormal columns, unit variance
-    X_train = pca.fit_transform(X_train)          # (n, r) where r = rank
-    X_test  = pca.transform(X_test)
-    n, d = X_train.shape
-    y_mean = y_train.mean()
-    y_train_c = y_train - y_mean
+for lam_val in lams:    
+    for data in datasets:
+        all_mses, all_lams = {}, {}
+        if data == 'wine_red':
+            X, y = load_wine_quality(red=True)
+        elif data == 'wine_white':
+            X, y = load_wine_quality(red=False)
+        elif data == 'housing':
+            X, y = load_cali_housing()
 
-    rng = np.random.default_rng(RANDOM_SEED)
+        # Train/test split and standardize X (fit on train)
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=TEST_SIZE, random_state=RANDOM_SEED
+        )
+        scaler = StandardScaler().fit(X_train)
+        X_train = scaler.transform(X_train)
+        X_test  = scaler.transform(X_test)
 
-    XtX = X_train.T @ X_train
-    Xty = X_train.T @ y_train_c
-    w_ref = solve(XtX, Xty)
+        pca = PCA(whiten=True, random_state=RANDOM_SEED)  # orthonormal columns, unit variance
+        X_train = pca.fit_transform(X_train)          # (n, r) where r = rank
+        X_test  = pca.transform(X_test)
+        n, d = X_train.shape
+        y_mean = y_train.mean()
+        y_train_c = y_train - y_mean
 
-    resid = y_train_c - X_train @ w_ref
-    sigma2_hat = float((resid @ resid) / (n-d)) # divide by df
-    y_pred_base = ridge_pred(X_test, w_ref) + y_mean
-    base_mse = mean_squared_error(y_pred_base, y_test)
-    print('non-private baseline mse: ', base_mse)
+        rng = np.random.default_rng(RANDOM_SEED)
 
-    opt_ws = []
-    for dim_ind in range(d):
-        ridge_model_no_intercept = Ridge(alpha=0, fit_intercept=False)
-        ridge_model_no_intercept.fit(np.transpose(
-            np.atleast_2d(X_train[:, dim_ind])), y_train_c)
-        opt_ws.append(ridge_model_no_intercept.coef_[0])
+        XtX = X_train.T @ X_train
+        Xty = X_train.T @ y_train_c
+        w_ref = solve(XtX, Xty)
 
-    opt_lams = [sigma2_hat/opt_ws[ind]**2 for ind in range(len(opt_ws))]
-    base_variances = {}
-    subsets = []
-    all_lams[0] = opt_lams
-    for dim_ind in range(d):
-        print(f'dim ind {dim_ind} {d}')
-        opt_lam0 = opt_lams[dim_ind]
-        ws = []
-        for num_subsets in range(NUM_SUBSETS):
-            pts = poisson_sample(X_train)
-            subsets.append(pts)
-            X_subset, Y_subset = X_train[pts], y_train_c[pts]
-            ws.append(ridge_1d(X_subset[:, dim_ind], Y_subset, opt_lam0))
-        base_variances[dim_ind] = np.var(ws)
-    pickle.dump(base_variances, open(f'data/baseline_{data}_variances.pkl', 'wb'))
+        resid = y_train_c - X_train @ w_ref
+        sigma2_hat = float((resid @ resid) / (n-d)) # divide by df
+        y_pred_base = ridge_pred(X_test, w_ref) + y_mean
+        base_mse = mean_squared_error(y_pred_base, y_test)
+        print('non-private baseline mse: ', base_mse)
 
-    for C in C_values:
-        priv_obl_mses = []
-        for trial in range(NUM_TRIALS):
-            print(trial)
-            unnoised_ws = []
-            release = []
-            for dim_ind in range(d):
-                chosen_pts = subsets[np.random.choice(range(len(subsets)))]
-                opt_lam0 = opt_lams[dim_ind]
-                w = ridge_1d(
-                    X_train[chosen_pts][:, dim_ind],
-                    y_train_c[chosen_pts], opt_lam0)
-                w += np.random.normal(0, np.sqrt(C*base_variances[dim_ind]))
-                release.append(w)
-            y_pred_closed = ridge_pred(X_test, release) + y_mean
-            priv_obl_mses.append(mean_squared_error(y_pred_closed, y_test))
-        print(f'C={C}, priv oblivious mse: ', np.mean(priv_obl_mses))
-
-
-        variances = {}
-        priv_aware_lam = [(C+1) * opt_lams[dim_ind] for dim_ind in range(len(opt_lams))]
-        all_lams[C] = priv_aware_lam
+        opt_ws = []
         for dim_ind in range(d):
+            ridge_model_no_intercept = Ridge(alpha=0, fit_intercept=False)
+            ridge_model_no_intercept.fit(np.transpose(
+                np.atleast_2d(X_train[:, dim_ind])), y_train_c)
+            opt_ws.append(ridge_model_no_intercept.coef_[0])
+
+        if type(lam_val) == tuple:
+            assert lam_val[0] == 'exact'
+            mi_to_opt = lam_val[1]
+            if mi_to_opt != 0:
+                C = 1/(2*mi_to_opt)
+            else:
+                C = 0
+            opt_lams = [(C+1)*sigma2_hat/opt_ws[ind]**2 for ind in range(len(opt_ws))]
+        else:
+            opt_lams = [lam_val for ind in range(len(opt_ws))]
+        base_variances = {}
+        subsets = {}
+        all_lams[0] = opt_lams
+        for dim_ind in range(d):
+            opt_lam0 = opt_lams[dim_ind]
             ws = []
-            for subset_ind in range(NUM_SUBSETS):
-                pts = subsets[subset_ind]
+            subsets[dim_ind] = []
+            for num_subsets in range(NUM_SUBSETS):
+                pts = poisson_sample(X_train)
+                subsets[dim_ind].append(pts)
                 X_subset, Y_subset = X_train[pts], y_train_c[pts]
-                ws.append(ridge_1d(X_subset[:, dim_ind], Y_subset, priv_aware_lam[dim_ind]))
-            variances[dim_ind] = np.var(ws)
-        pickle.dump(variances, open(f'data/C={C}_{data}_variances.pkl', 'wb'))
+                ws.append(ridge_1d(X_subset[:, dim_ind], Y_subset, opt_lam0))
+            base_variances[dim_ind] = np.var(ws, ddof=1)
+        pickle.dump(base_variances, open(f'data/baseline_{data}_{lam_val}_variances.pkl', 'wb'))
 
-        priv_aware_mses = []
-        for trial in range(NUM_TRIALS):
-            print(trial)
-            chosen_pts = subsets[np.random.choice(range(len(subsets)))]
-            unnoised_ws = []
-            release = []
+        for C in C_values:
+            priv_obl_mses = []
+            for trial in range(NUM_TRIALS):
+                # print(trial)
+                unnoised_ws = []
+                release = []
+                for dim_ind in range(d):
+                    chosen_pts = subsets[dim_ind][np.random.choice(range(NUM_SUBSETS))]
+                    opt_lam0 = opt_lams[dim_ind]
+                    w = ridge_1d(
+                        X_train[chosen_pts][:, dim_ind],
+                        y_train_c[chosen_pts], opt_lam0)
+                    w += np.random.normal(0, np.sqrt(C*base_variances[dim_ind]))
+                    release.append(w)
+                y_pred_closed = ridge_pred(X_test, release) + y_mean
+                priv_obl_mses.append(mean_squared_error(y_pred_closed, y_test))
+            # print(f'C={C}, priv oblivious mse: ', np.mean(priv_obl_mses))
+
+
+            variances = {}
+            if type(lam_val) == tuple:
+                assert lam_val[0] == 'exact'
+                mi_to_opt = lam_val[1]
+                if mi_to_opt != 0:
+                    correction_factor = (2*mi_to_opt)
+            else:
+                correction_factor = 0
+            correction_factor += 1
+            priv_aware_lam = [(C+1) * correction_factor*opt_lams[dim_ind] for dim_ind in range(len(opt_lams))]
+            all_lams[C] = priv_aware_lam
             for dim_ind in range(d):
-                w = ridge_1d(
-                    X_train[chosen_pts][:, dim_ind],
-                    y_train_c[chosen_pts], priv_aware_lam[dim_ind])
-                w += np.random.normal(0, np.sqrt(C*variances[dim_ind]))
-                release.append(w)
-            y_pred_closed = ridge_pred(X_test, release) + y_mean
-            priv_aware_mses.append(mean_squared_error(y_pred_closed, y_test))
-        print(f'C={C}, priv aware mse: ', np.mean(priv_aware_mses))
-        all_mses[C] = (priv_obl_mses, priv_aware_mses)
+                ws = []
+                dim_subsets = subsets[dim_ind]
+                for subset_ind in range(NUM_SUBSETS):
+                    pts = dim_subsets[subset_ind]
+                    X_subset, Y_subset = X_train[pts], y_train_c[pts]
+                    ws.append(ridge_1d(X_subset[:, dim_ind], Y_subset, priv_aware_lam[dim_ind]))
+                variances[dim_ind] = np.var(ws, ddof=1)
+            pickle.dump(variances, open(f'data/C={C}_{data}_{lam_val}_variances.pkl', 'wb'))
 
-        pickle.dump(all_mses, open(f'data/C={C}_{data}_mses.pkl', 'wb'))
-        pickle.dump(all_lams, open(f'data/C={C}_{data}_lams.pkl', 'wb'))
+            priv_aware_mses = []
+            for trial in range(NUM_TRIALS):
+                print(trial)
+                unnoised_ws = []
+                release = []
+                for dim_ind in range(d):
+                    chosen_pts = subsets[dim_ind][np.random.choice(range(NUM_SUBSETS))]
+                    w = ridge_1d(
+                        X_train[chosen_pts][:, dim_ind],
+                        y_train_c[chosen_pts], priv_aware_lam[dim_ind])
+                    w += np.random.normal(0, np.sqrt(C*variances[dim_ind]))
+                    release.append(w)
+                y_pred_closed = ridge_pred(X_test, release) + y_mean
+                priv_aware_mses.append(mean_squared_error(y_pred_closed, y_test))
+            print(f'C={C}, priv aware mse: ', np.mean(priv_aware_mses))
+            all_mses[C] = (priv_obl_mses, priv_aware_mses)
+
+        pickle.dump(all_mses, open(f'data/{data}_{lam_val}_mses.pkl', 'wb'))
+        pickle.dump(all_lams, open(f'data/{data}_{lam_val}_lams.pkl', 'wb'))
--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ from sklearn.linear_model import Ridge
 from typing import Tuple, Optional, Sequence
 from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
 from sklearn.compose import ColumnTransformer
-
+from sklearn.datasets import fetch_california_housing
 def load_wine_quality(red=True):
     if red:
         url = "winequality-red.csv"
@@ -20,142 +20,8 @@ def load_wine_quality(red=True):
 
     return X, y
 
-def load_adult_census(
-    csv_path: str,
-    *,
-    label_candidates: Sequence[str] = ("income", "class", "target", "salary"),
-    positive_labels: Sequence[str] = (">50K", ">50K.", "1", " >50K", " >50K.", ">=50K"),
-    negative_labels: Sequence[str] = ("<=50K", "<=50K.", "0", " <=50K", " <=50K."),
-    assume_missing_headers: bool = True,
-    drop_na: bool = True,
-    one_hot_drop_first: bool = True
-) -> Tuple[np.ndarray, np.ndarray]:
-    """
-    Load the Adult (Census Income) dataset from a CSV and return (X, y).
-
-    - Handles typical 'income' label variations (>50K vs <=50K; trailing periods).
-    - Replaces '?' with NaN; optionally drops rows with any NaNs.
-    - One-hot encodes categorical columns so X is numeric (float).
-    - If the CSV has no headers, sets the canonical Adult column names.
-
-    Returns:
-        X: np.ndarray[float]  (one-hot encoded features)
-        y: np.ndarray[float]  (binary: 1 for >50K, 0 otherwise)
-    """
-    # Canonical Adult column names (UCI order)
-    canonical_cols = [
-        "age",
-        "workclass",
-        "fnlwgt",
-        "education",
-        "education_num",
-        "marital_status",
-        "occupation",
-        "relationship",
-        "race",
-        "sex",
-        "capital_gain",
-        "capital_loss",
-        "hours_per_week",
-        "native_country",
-        "income",  # label
-    ]
-
-    # Try reading with headers first
-    try:
-        df = pd.read_csv(csv_path, na_values="?", skipinitialspace=True)
-    except Exception:
-        # Fallback to semicolon or other separators if needed
-        df = pd.read_csv(csv_path, sep=",", na_values="?", skipinitialspace=True)
-
-    # If the file likely lacks headers, set canonical ones (when column count matches)
-    if assume_missing_headers and df.columns.dtype == "int64":
-        # Pandas may assign integer headers if none were present
-        if df.shape[1] in (14, 15):  # 15 incl. label; 14 when label separated/unknown
-            cols = canonical_cols[: df.shape[1]]
-            df.columns = cols
-    elif assume_missing_headers and not set(df.columns).intersection(label_candidates):
-        # If no known label name but col count matches, apply canonical names
-        if df.shape[1] in (14, 15):
-            df.columns = canonical_cols[: df.shape[1]]
-
-    # Normalize whitespace in string columns
-    for c in df.select_dtypes(include=["object"]).columns:
-        df[c] = df[c].astype(str).str.strip().replace({"?": np.nan})
-
-    # Find the label column
-    label_col: Optional[str] = None
-    for cand in label_candidates:
-        if cand in df.columns:
-            label_col = cand
-            break
-    if label_col is None:
-        raise ValueError(
-            f"Could not find a label column. Looked for {label_candidates}. "
-            "Pass a CSV with an 'income' (or similar) column."
-        )
-
-    # Build y (binary 0/1)
-    y_raw = df[label_col].astype(str).str.strip()
-    y = y_raw.apply(lambda v: 1.0 if v in positive_labels else (0.0 if v in negative_labels else np.nan))
-
-    # Drop rows with unknown/NaN label
-    mask_valid_y = ~y.isna()
-    df = df.loc[mask_valid_y].copy()
-    y = y.loc[mask_valid_y].astype(float).to_numpy()
-
-    # Features = everything except label
-    X_df = df.drop(columns=[label_col])
-
-    # Handle missing values before encoding (optional)
-    # (Adult has '?' placeholders—already mapped to NaN above.)
-    if drop_na:
-        # If dropping, make sure to keep y aligned
-        before = len(X_df)
-        keep_mask = ~X_df.isna().any(axis=1)
-        X_df = X_df.loc[keep_mask]
-        y = y[keep_mask.to_numpy()]
-        # If you want to know how many dropped:
-        # print(f"Dropped {before - len(X_df)} rows due to NaNs")
-
-    # One-hot encode categoricals, keep numeric as-is
-    cat_cols = X_df.select_dtypes(include=["object"]).columns.tolist()
-    if cat_cols:
-        X_df = pd.get_dummies(X_df, columns=cat_cols, drop_first=one_hot_drop_first)
-
-    # Ensure float dtype
-    X = X_df.to_numpy(dtype=float)
-
-    return X, y
-
-def load_wine_quality_red(csv_path: str):
-    df = pd.read_csv(csv_path, sep=";")
-    if "quality" not in df.columns:
-        raise ValueError("Expected a 'quality' column in winequality-red.csv")
-    y = df["quality"].to_numpy(dtype=float)
-    X = df.drop(columns=["quality"]).to_numpy(dtype=float)
-    return X, y
-
-def load_bank():
-    bank_marketing = pd.read_csv('pac_private_gd/bank/bank-full.csv', sep=";")
-    # data (as pandas dataframes) 
-    X = bank_marketing.drop(columns=["y"])   # DataFrame of features (categorical + numeric)
-    y = bank_marketing["y"]                  # Series target: "yes"/"no"
-    y = (y.astype(str).str.lower() == "yes").astype(int).to_numpy()
-
-    cat_cols = X.select_dtypes(include=["object", "category"]).columns
-    num_cols = X.columns.difference(cat_cols)
-
-    ct = ColumnTransformer(
-        transformers=[
-            ("num", StandardScaler(with_mean=True, with_std=True), list(num_cols)),
-            ("cat", OneHotEncoder(handle_unknown="ignore", sparse_output=False), list(cat_cols)),
-        ],
-        remainder="drop",
-    )
-    X = ct.fit_transform(X)
-    return X, y
-
+def load_cali_housing():
+    return fetch_california_housing(return_X_y=True)
 
 def ridge_1d(x, y, lam):
     h_s = sum([x[i]**2 for i in range(len(x))])
