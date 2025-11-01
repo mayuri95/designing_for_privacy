90e2b99e58bdab13c0e7da9bb3d5d7b2d0897d42
Mayuri <mayuri.sridhar@gmail.com>
2025-11-01 10:42:29 -0400

    fixes to choose mu and main to run across e0

--- pac_private_gd/choose_mu.py
+++ pac_private_gd/choose_mu.py
@@ -16,28 +16,27 @@ num_trials = 1
 
 mus = [1e-3, 1e-2, 1e-1, 1, 10]
 T=50
+for dataset in ['credit', 'mnist_7_vs_9']:
+    for mu in mus:
+        X, y, X_test, y_test, num_classes = data.load_dataset(dataset)
+        e0 = utils.find_e0(X, y, num_classes, mu)
+        n, d = X.shape
+        L = est_L(X, mu)
+        print(f'L={L}')
+        num_clips = 0
+        model = LinearModel(d, 1)
+        loss_fn = torch.nn.BCEWithLogitsLoss()
+        per_sample_grads = utils.get_per_sample_grads(model, loss_fn, X, y, l2_lambda=mu).cpu().numpy()
+        for d_i in range(d):
 
-C=1
-for mu in mus:
-    X, y, X_test, y_test, num_classes = data.load_dataset('credit')
-    e0 = utils.find_e0(X, y, num_classes, mu)
-    n, d = X.shape
-    L = est_L(X, mu)
-    print(f'L={L}')
-    num_clips = 0
-    model = LinearModel(d, 1)
-    loss_fn = torch.nn.BCEWithLogitsLoss()
-    per_sample_grads = utils.get_per_sample_grads(model, loss_fn, X, y, mu).cpu().numpy()
-    
-    for d_i in range(d):
-
-        def grad_i_fn(): # return a torch scalar
-            return per_sample_grads[np.random.rand(num_features) < 0.5, d_i].mean().item()
-                    
-        grad_i_var = utils.exact_var_1d(per_sample_grads[:, d_i])
-        eta_i = utils.optimal_eta(mu=mu, T=T, C=0, e0=e0[d_i], var=grad_i_var)
-        assert eta_i >= 0
-        eta_clip = np.clip(eta_i, 0, L)
-        if eta_clip != eta_i:
-            num_clips += 1
-    print(f'mu: {mu}, % clipped dims:{100*(num_clips/d)}')
+            def grad_i_fn(): # return a torch scalar
+                return per_sample_grads[np.random.rand(num_features) < 0.5, d_i].mean().item()
+                        
+            grad_i_var = utils.exact_var_1d(per_sample_grads[:, d_i])
+            eta_i = utils.optimal_eta(mu=mu, T=T, C=0, e0=e0[d_i], var=grad_i_var)
+            assert eta_i >= 0
+            eta_clip = np.clip(eta_i, 0, L)
+            if eta_clip != eta_i:
+                num_clips += 1
+        print(f'dataset: {dataset} mu: {mu}, % clipped dims:{100*(num_clips/d)}')
+        print('-------')
--- pac_private_gd/main.py
+++ pac_private_gd/main.py
@@ -18,22 +18,20 @@ budget_list = [None, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
 budget_list = [budget_list[int(sys.argv[1])]]
 T_list = [50]
 num_trials = 1
-
-mus = [1e-3, 1e-2, 1e-1, 1, 10]
-mus = [1]
+mu = 1.
 T=50
+e0_type_list = ['exact', 0.001, 0.01, 0.1]
 
 for dataset in dataset_list:
     X, y, X_test, y_test, num_classes = data.load_dataset(dataset)
-
+    e0 = find_e0(X, y, num_classes, mu)
     for inv_mi_budget in budget_list:
         d = {}
-        for mu in mus:
-            e0 = find_e0(X, y, num_classes, mu)
-            d[mu] = {}
+        for e0_type in e0_type_list:
+            d[e0_type] = {}
             for privacy_aware in [True, False]:
                 accs = []
-                for _ in range(num_trials):
+                for trial_ind in range(num_trials):
                     train_loss, cla_loss, test_acc = pac_private_gd(
                         X=X,
                         y=y,
@@ -44,11 +42,12 @@ for dataset in dataset_list:
                         T=T,
                         mi_budget=1/inv_mi_budget if inv_mi_budget is not None else None,
                         privacy_aware=privacy_aware,
-                        e0=e0,
+                        e0=e0 if e0_type == 'exact' else np.ones_like(e0) * e0_type,
                         verbose=False
                     )
+                    print(trial_ind)
                     accs.append(test_acc)
-                d[mu][privacy_aware] = accs
+                d[e0_type][privacy_aware] = accs
                 print(privacy_aware, inv_mi_budget, np.average(accs), np.std(accs))
         fname = 'results/credit_data_budget={}.pkl'.format(inv_mi_budget)
         pickle.dump(d, open(fname, 'wb'))
