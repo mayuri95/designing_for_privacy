7a14b6d5b50eb917b7c47c1a969acf782db53a44
Xiaochen Zhu <zhuxiaochen1005@gmail.com>
2025-11-06 16:01:48 -0500

    add dp linear regression baseline

--- dp.py
+++ dp.py
@@ -1,4 +1,10 @@
 import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.datasets import fetch_california_housing
+import json
 
 def adassp(X, y, opts):
     BX = np.max(np.linalg.norm(X, axis=1))
@@ -34,3 +40,58 @@ def adassp(X, y, opts):
     thetahat = np.linalg.solve(XTXhat + lamb * np.eye(d), XTyhat)
 
     return thetahat
+
+num_trials = 1000
+res_dict = {}
+# res_dict[dataset][eps] = mean mse
+for dataset in ['wine_quality_red', 'wine_quality_white', 'cali_housing']:
+    res_dict[dataset] = {}
+    def load_wine_quality(red=True):
+        if red:
+            url = "winequality-red.csv"
+        else:
+            url = "winequality-white.csv"
+        df= pd.read_csv(url, sep=';')
+        X = df.drop("quality", axis=1).to_numpy(dtype=float)
+        y = df["quality"].to_numpy(dtype=float)
+
+        return X, y
+
+    def load_cali_housing():
+        return fetch_california_housing(return_X_y=True)
+
+    if dataset == 'wine_quality_red':
+        X, y = load_wine_quality(red=True)
+    elif dataset == 'wine_quality_white':
+        X, y = load_wine_quality(red=False)
+    else:
+        X, y = load_cali_housing()
+
+    # Train/test split and standardize X (fit on train)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+    scaler = StandardScaler().fit(X_train)
+    X_train = scaler.transform(X_train)
+    X_test  = scaler.transform(X_test)
+
+    pca = PCA(whiten=True, random_state=42)  # orthonormal columns, unit variance
+    X_train = pca.fit_transform(X_train)          # (n, r) where r = rank
+    X_test  = pca.transform(X_test)
+
+    y_mean = y_train.mean(axis=0)
+    y_train = y_train - y_mean
+    y_test  = y_test - y_mean
+    for eps in [1.6426117097961406,0.7304317044395013,0.3563228120191924,0.177101450509287,0.08841755656932042]:
+        mses = []
+        for i in range(num_trials):
+            w = adassp(X_train, y_train, {'eps': eps, 'delta': 1e-5})
+            mse = w @ X_test.T - y_test
+            mse = (mse ** 2).mean()
+            mses.append(mse)
+        
+        res_dict[dataset][eps] = (np.mean(mses), np.std(mses))
+    
+with open('dp_adassp_results.json', 'w') as f:
+    json.dump(res_dict, f)
+    
