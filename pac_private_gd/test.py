f180123991147dab094adea0b140f88bebe6161f
Mayuri Sridhar <mayuri@Mayuris-MacBook-Air-2.local>
2025-11-06 22:17:16 -0500

    add rr results, figs, notebooks

---
+++ pac_private_gd/test.py
@@ -0,0 +1,49 @@
+import os
+import numpy as np
+from pac_private_gd import pac_private_gd
+from utils import find_e0
+import pandas as pd
+import random
+import string
+import os
+import data
+import pickle
+import sys
+
+# run as budget ind, e0 ind, dataset ind
+budget_list = [None] 
+T_list = [50]
+num_trials = 1
+mu = 1.
+T=50
+e0_type_list = ['exact']
+dataset_list = [
+    'mnist_7_vs_9',
+]
+
+for dataset in dataset_list:
+    print(dataset)
+    X, y, X_test, y_test, num_classes = data.load_dataset(dataset)
+    e0 = find_e0(X, y, num_classes, mu)
+    print(np.linalg.norm(e0))
+    for inv_mi_budget in budget_list:
+        for e0_type in e0_type_list:
+            d = {}
+            for privacy_aware in [True, False]:
+                accs = []
+                for trial_ind in range(num_trials):
+                    train_loss, cla_loss, test_acc = pac_private_gd(
+                        X=X,
+                        y=y,
+                        X_test=X_test,
+                        y_test=y_test,
+                        num_classes=num_classes,
+                        mu=mu,
+                        T=T,
+                        mi_budget=1/inv_mi_budget if inv_mi_budget is not None else None,
+                        privacy_aware=privacy_aware,
+                        e0=e0 if e0_type == 'exact' else np.ones_like(e0) * e0_type,
+                        verbose=False
+                    )
+                    print(trial_ind, test_acc)
+
