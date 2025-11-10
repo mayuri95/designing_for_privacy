2e1a12b72c8c51bbd58a995547e7cd808d61a226
Xiaochen Zhu <zhuxiaochen1005@gmail.com>
2025-11-10 13:12:56 -0500

    new dp results with epsilon/2

--- dp.py
+++ dp.py
@@ -17,25 +17,24 @@ def adassp(X, y, opts):
     varrho = 0.05
 
     # set the eigenvalue limit
-    eta = np.sqrt(d * np.log(6 / delta) * np.log(2 * d**2 / varrho)) * BX**2 / (epsilon / 3)
+    eta = np.sqrt(d * np.log(4 / delta) * np.log(2 * d**2 / varrho)) * BX**2 / (epsilon / 2)
 
     XTy = X.T @ y
     XTX = X.T @ X + np.eye(d)
 
     S = np.linalg.svd(XTX, compute_uv=False)
-    logsod = np.log(6 / delta)
 
-    lamb_min = S[-1] + np.random.randn() * BX**2 * np.sqrt(logsod) / (epsilon / 3) - logsod / (epsilon / 3)
+    lamb_min = S[-1] # + np.random.randn() * BX**2 * np.sqrt(logsod) / (epsilon / 3) - logsod / (epsilon / 3)
     lamb_min = max(lamb_min, 0)
 
     lamb = max(0, eta - lamb_min)
 
-    XTyhat = XTy + (np.sqrt(np.log(6 / delta)) / (epsilon / 3)) * BX * BY * np.random.randn(d)
+    XTyhat = XTy + (np.sqrt(np.log(4 / delta)) / (epsilon / 2)) * BX * BY * np.random.randn(d)
 
     # generate symmetric Gaussian noise
     Z = np.random.randn(d, d)
     Z = 0.5 * (Z + Z.T)
-    XTXhat = XTX + (np.sqrt(np.log(6 / delta)) / (epsilon / 3)) * BX**2 * Z
+    XTXhat = XTX + (np.sqrt(np.log(4 / delta)) / (epsilon / 2)) * BX**2 * Z
 
     thetahat = np.linalg.solve(XTXhat + lamb * np.eye(d), XTyhat)
 
