68fc44a0c6b39e4c84a796f8b73251ccf5fa43a7
Xiaochen Zhu <zhuxiaochen1005@gmail.com>
2026-03-11 15:44:02 -0400

    typo

--- pac_private_gd/commands_60.sh
+++ pac_private_gd/commands_60.sh
@@ -15,7 +15,7 @@ for a in {0..9}; do
             VECLIB_MAXIMUM_THREADS=1 \
             NUMEXPR_NUM_THREADS=1 \
             TORCH_NUM_THREADS=1 \
-        uv run python3 -u "$a" "$b" "$c" &
+        uv run python3 -u main.py "$a" "$b" "$c" &
 
       core=$((core + 1))
     done
